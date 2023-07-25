e5fdf217c8c6829c6bead4b143406b2d4830209f
aleacos9 <aleacos9@gmail.com>
2023-07-25 10:28:30 -0300

    * se agrega en saldo actual en la operación cuenta corriente del perfil tutor * se termina la operación deuda corriente para el perfil tutor

--- metadatos/permisos/grupo_acceso__tutor.sql
+++ metadatos/permisos/grupo_acceso__tutor.sql
@@ -67,10 +67,4 @@ INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, i
 	NULL, --item_id
 	'82000025'  --item
 );
-INSERT INTO apex_usuario_grupo_acc_item (proyecto, usuario_grupo_acc, item_id, item) VALUES (
-	'gestionescuelas', --proyecto
-	'tutor', --usuario_grupo_acc
-	NULL, --item_id
-	'82000026'  --item
-);
 --- FIN Grupo de desarrollo 82
--- php/tutor/deuda_corriente/ci_deuda_corriente.php
+++ php/tutor/deuda_corriente/ci_deuda_corriente.php
@@ -1,19 +1,26 @@
 <?php
 class ci_deuda_corriente extends ci_cuenta_corriente
 {
+    protected $s__datos_deuda_corriente = array();
+
+    public function cargar_datos()
+    {
+        if (!empty($this->s__alumno_editar)) {
+            $persona = new persona($this->s__alumno_editar);
+            $this->s__datos_deuda_corriente = $persona->get_datos_deuda_corriente();
+            $this->s__nombre_alumno = $persona->get_nombre_completo_alumno();
+        }
+    }
 
     //---- cuadro_deuda_corriente ------------------------------------------------------
 
     function conf__cuadro_deuda_corriente($cuadro)
     {
         if ($this->s__alumno_editar) {
-            $cuadro->set_titulo('<div class="titulo_alumno">'.$this->s__nombre_alumno.'</div>');
+            $cuadro->set_titulo('<div class="titulo_alumno">' . $this->s__nombre_alumno . '</div>');
         }
-        /*ei_arbol($this->s__datos_filtro);
-        if (isset($this->s__datos_filtro)) {*/
-            $this->s__datos_filtro['id_persona'] = $this->s__alumno_editar;
-            $cuadro->set_datos(dao_personas::get_deuda_actual_por_alumno($this->s__datos_filtro));
-        //}
+        if (isset($this->s__datos_deuda_corriente)) {
+            $cuadro->set_datos($this->s__datos_deuda_corriente);
+        }
     }
-
 }
