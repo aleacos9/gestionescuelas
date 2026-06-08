2f686ca9f260f322965ac2b741a6ad307b2e7dcb
aleacos9 <aleacos9@gmail.com>
2026-06-08 13:34:43 -0300

    * fix envio de correo electronico en la nueva operacion: Listado de rechazos de dÃ©bitos bancarios

--- php/listados/listado_rechazos/ci_listado_rechazos.php
+++ php/listados/listado_rechazos/ci_listado_rechazos.php
@@ -35,7 +35,7 @@ class ci_listado_rechazos extends ci_abm_personas
     {
         if (isset($this->s__datos_filtro)) {
             $datos = dao_consultas::get_listado_rechazos_debitos($this->s__datos_filtro);
-            
+
             foreach ($datos as $id => $fila) {
                 // 1. Columna notificado_mail
                 if ($fila['notificado_mail'] == 'SI') {
@@ -58,17 +58,17 @@ class ci_listado_rechazos extends ci_abm_personas
                     $msj = envio_correo::generar_mensaje_whatsapp_rechazo($fila);
                     $msj_url = rawurlencode($msj);
                     $url_wa = "https://wa.me/{$tel_limpio}?text={$msj_url}";
-                    
-                    $wa_accion = "<a href='#' onclick=\"{$this->objeto_js}.registrar_whatsapp('{$fila['id_transaccion_cc']}', '{$url_wa}'); return false;\" style='margin-left: 8px;' title='Enviar WhatsApp'>".
-                                 "<img src='https://cdn-icons-png.flaticon.com/24/3670/3670051.png' width='16' height='16' style='vertical-align: middle;'>".
-                                 "</a>";
+
+                    $wa_accion = "<a href='#' onclick=\"{$this->objeto_js}.registrar_whatsapp('{$fila['id_transaccion_cc']}', '{$url_wa}'); return false;\" style='margin-left: 8px;' title='Enviar WhatsApp'>" .
+                        "<img src='https://cdn-icons-png.flaticon.com/24/3670/3670051.png' width='16' height='16' style='vertical-align: middle;'>" .
+                        "</a>";
                 }
 
-                $datos[$id]['notificado_whatsapp'] = "<div style='display: flex; align-items: center;'>".
-                                                     "<span class='estado-wa'>$img_wa_estado</span>".
-                                                     "$wa_accion</div>";
+                $datos[$id]['notificado_whatsapp'] = "<div style='display: flex; align-items: center;'>" .
+                    "<span class='estado-wa'>$img_wa_estado</span>" .
+                    "$wa_accion</div>";
             }
-            
+
             $cuadro->set_datos($datos);
         }
     }
@@ -82,12 +82,16 @@ class ci_listado_rechazos extends ci_abm_personas
     }
 
     function evt__cuadro__seleccion($seleccion)
-	{
+    {
         $this->s__datos_seleccion = $seleccion;
-	}
+    }
 
     function evt__notificar()
     {
+        // Como el evento 'notificar' en Toba Editor tiene 'Maneja Datos = NO', 
+        // el framework omite procesar la seleccion del cuadro. Forzamos su procesamiento manual.
+        $this->dep('cuadro')->disparar_eventos();
+
         if (empty($this->s__datos_seleccion)) {
             toba::notificacion()->agregar("Debe seleccionar al menos un registro para notificar.", "error");
             return;
@@ -99,7 +103,7 @@ class ci_listado_rechazos extends ci_abm_personas
             $datos_indexados[$d['id_transaccion_cc']] = $d;
         }
 
-        require_once(toba::proyecto()->get_path_php().'/utiles/envio_correo.php');
+        require_once(toba::proyecto()->get_path_php() . '/utiles/envio_correo.php');
         $enviados = 0;
         $errores = array();
         $lista_enviados = array();
@@ -108,7 +112,7 @@ class ci_listado_rechazos extends ci_abm_personas
             $id_cc = $seleccion['id_transaccion_cc'];
             if (!isset($datos_indexados[$id_cc])) continue;
             $fila = $datos_indexados[$id_cc];
-            
+
             try {
                 if (empty($fila['tutor_email'])) {
                     $errores[] = "El alumno {$fila['alumno']} no tiene email de tutor cargado.";
@@ -118,7 +122,7 @@ class ci_listado_rechazos extends ci_abm_personas
                 $correo = new envio_correo($fila['tutor_email']);
                 $correo->set_asunto(envio_correo::generar_asunto_notificacion_rechazo());
                 $correo->set_cuerpo(envio_correo::generar_cuerpo_notificacion_rechazo($fila));
-                
+
                 if ($correo->enviar()) {
                     dao_consultas::set_notificacion_mail($id_cc);
                     $enviados++;
@@ -137,16 +141,26 @@ class ci_listado_rechazos extends ci_abm_personas
         }
     }
 
-    
+
     function extender_objeto_js()
     {
         $id_js = $this->objeto_js;
+        $id_js_cuadro = $this->dep('cuadro')->objeto_js;
         echo "
             {$id_js}.evt__notificar = function() {
-                if ($('input[id*=\"82000084\"][id$=\"_seleccion\"]:checked').length == 0) {
+                var cuadro = {$id_js_cuadro};
+                var seleccionados = cuadro.get_ids_seleccionados('seleccion');
+                
+                if (seleccionados.length == 0) {
                     alert('Debe seleccionar al menos un registro para notificar.');
                     return false;
                 }
+
+                // Toba no envia automaticamente el evento multiple del cuadro si el submit 
+                // se dispara desde un boton del CI. Seteamos manualmente el evento del cuadro 
+                // para que PHP invoque evt__cuadro__seleccion() antes de evt__notificar().
+                document.getElementById(cuadro._input_submit).value = 'seleccion';
+                
                 return true;
             }
 
@@ -158,22 +172,22 @@ class ci_listado_rechazos extends ci_abm_personas
             }
 
             {$id_js}.seleccionar_todos = function() {
-                $('input[id*=\"82000084\"][id$=\"_seleccion\"]:not(:disabled)').prop('checked', true).change();
+                {$id_js_cuadro}.seleccionar_todos('seleccion');
             }
             {$id_js}.deseleccionar_todos = function() {
-                $('input[id*=\"82000084\"][id$=\"_seleccion\"]').prop('checked', false).change();
+                {$id_js_cuadro}.deseleccionar_todos('seleccion');
             }
 
             $(function() {
                 var barra = $('<div style=\"padding: 5px; text-align: right; font-size: 11px;\"><a href=\"#\" id=\"lnk_sel_todos\" style=\"color: #2196F3; text-decoration: none; font-weight: bold;\">Seleccionar Todos</a> | <a href=\"#\" id=\"lnk_sel_ninguno\" style=\"color: #f44336; text-decoration: none; font-weight: bold;\">Ninguno</a></div>');
                 $('#cuerpo_js_cuadro_82000084_cuadro').before(barra);
-                
+
                 $('#lnk_sel_todos').click(function() { {$id_js}.seleccionar_todos(); return false; });
                 $('#lnk_sel_ninguno').click(function() { {$id_js}.deseleccionar_todos(); return false; });
 
                 var cont = $('#cuerpo_js_cuadro_82000084_cuadro');
                 if (cont.length > 0) {
-                    cont.find('input[id$=_seleccion]').each(function() {
+                    cont.find('input[id\$=\"_seleccion\"]').each(function() {
                         var check = $(this);
                         var fila = check.closest('tr');
                         var mail_notificado = fila.find('.estado-mail img[src*=\"aplicar.png\"]').length > 0;
@@ -187,4 +201,3 @@ class ci_listado_rechazos extends ci_abm_personas
         ";
     }
 }
-?>
